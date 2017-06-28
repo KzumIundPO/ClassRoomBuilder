8bec2b01bc8c3cf350a0c5789797017ed5b586b5
aletuna <janis.hanser@gmail.com>
2017-06-28 17:35:54 +0200

    Fixed server.js template for deplyment

--- deploy_util/server.js
+++ deploy_util/server.js
@@ -1,3 +1,4 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
+connect().use(serveStatic(__dirname)).listen(8100);
+console.log("listening on port 8100");
